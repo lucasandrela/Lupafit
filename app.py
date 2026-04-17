13c99eff2c6c8174534e73383a6d16159fc11a97
lucasandrela <lucasadrela.senai@gmail.com>
2026-04-17 10:02:27 -0300

    altera método da rota /catraca/validar de POST para GET

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def login():
 
     return jsonify({"error": "Credenciais inválidas."}), 401
 
-@app.route('/catraca/validar', methods=['POST'])
+@app.route('/catraca/validar', methods=['GET'])
 def validar_acesso():
     dados = request.get_json()
     cpf_bruto = dados.get("cpf", "")
